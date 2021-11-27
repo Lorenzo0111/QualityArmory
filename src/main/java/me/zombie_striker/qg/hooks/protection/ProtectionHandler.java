d6eb4f1603c7fbd47db7165c72555467646e0d32
Lorenzo <69311874+Lorenzo0111@users.noreply.github.com>
2021-11-27 15:50:40 +0100

    Fixed the protection handler

--- src/main/java/me/zombie_striker/qg/hooks/protection/ProtectionHandler.java
+++ src/main/java/me/zombie_striker/qg/hooks/protection/ProtectionHandler.java
@@ -5,18 +5,32 @@ import me.zombie_striker.qg.hooks.protection.implementation.ResidenceHook;
 import me.zombie_striker.qg.hooks.protection.implementation.TownyHook;
 import me.zombie_striker.qg.hooks.protection.implementation.WorldGuardHook;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class ProtectionHandler {
     private final static Set<ProtectionHook> compatibilities = new HashSet<>();
 
     public static void init() {
-        hook("WorldGuard", WorldGuardHook::new);
-        hook("Towny", TownyHook::new);
-        hook("Residence", ResidenceHook::new);
+        Map<String,Class<? extends ProtectionHook>> classes = new HashMap<>();
+        classes.put("WorldGuard", WorldGuardHook.class);
+        classes.put("Towny", TownyHook.class);
+        classes.put("Residence", ResidenceHook.class);
+
+        for (Map.Entry<String,Class<? extends ProtectionHook>> entry : classes.entrySet()) {
+            try {
+                Constructor<? extends ProtectionHook> constructor = entry.getValue().getConstructor();
+
+                hook(entry.getKey(), constructor::newInstance);
+            } catch (Throwable ignored) {}
+        }
     }
 
     public static boolean canPvp(Location target) {
@@ -27,14 +41,16 @@ public class ProtectionHandler {
         return compatibilities.stream().allMatch(compatibility -> compatibility.canExplode(target));
     }
 
-    public static void hook(String plugin, CompatibilityConstructor constructor) {
+    public static void hook(String plugin, CompatibilityConstructor constructor) throws InvocationTargetException, InstantiationException, IllegalAccessException {
         if (Bukkit.getPluginManager().isPluginEnabled(plugin) && (boolean) QAMain.getInstance().a("hooks." + plugin, true)) {
             compatibilities.add(constructor.create());
+
+            QAMain.getInstance().getLogger().info("Hooked with " + plugin + "!");
         }
     }
 
     @FunctionalInterface
     private interface CompatibilityConstructor {
-        ProtectionHook create();
+        ProtectionHook create() throws InvocationTargetException, InstantiationException, IllegalAccessException;
     }
 }
